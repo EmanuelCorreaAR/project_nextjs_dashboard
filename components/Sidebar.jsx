0f02993de1d9be0a9229849bbf46fdccd52ecbdb
EmanuelCorreaAR <correa.emanuel@outlook.com>
2023-02-17 21:59:14 -0300

    component header-sidebar initial state

---
+++ components/Sidebar.jsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import Image from "next/link";
+import React from "react";
+import {RxSketchLogo, RxDashboard, RxPerson} from "react-icons/rx"
+import {HiOutlineShoppingBag} from "react-icons/hi"
+import {FiSettings} from "react-icons/fi"
+
+
+const Sidebar = ({ children }) => {
+  return (
+    <div className="flex">
+      <div className="fixed w-20 h-screen p-4 bg-white border-r-[1px] flex flex-col justify-between">
+        <div className="flex flex-col items-center">
+          <Link href="/">
+            <div className="bg-purple-800 text-white p-3 rounded-lg inline-block">
+                <RxSketchLogo size={30}/>
+            </div>
+          </Link>
+          <span className="border-b-[1px] border-gray-200 w-full p-2"></span>
+          <Link href="/">
+            <div className="bg-gray-300 hover:bg-gray-400 my-4 p-3 rounded-lg inline-block">
+                <RxDashboard size={30}/>
+            </div>
+          </Link>
+          <Link href="/">
+            <div className="bg-gray-300 hover:bg-gray-400 my-4 p-3 rounded-lg inline-block">
+                <RxPerson size={30}/>
+            </div>
+          </Link>
+          <Link href="/">
+            <div className="bg-gray-300 hover:bg-gray-400 my-4 p-3 rounded-lg inline-block">
+                <HiOutlineShoppingBag size={30}/>
+            </div>
+          </Link>
+          <Link href="/">
+            <div className="bg-gray-300 hover:bg-gray-400 my-4 p-3 rounded-lg inline-block">
+                <FiSettings size={30}/>
+            </div>
+          </Link>
+        </div>
+      </div>
+
+      <main className="ml-20 w-full">{children}</main>
+    </div>
+  );
+};
+
+export default Sidebar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
+import Sidebar from "../components/Sidebar.jsx"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Sidebar>
+      <Component {...pageProps} />
+    </Sidebar>
+  )
 }
