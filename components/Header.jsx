8b07002e4cda99a3d96cb8ec734bf13e9732efff
EmanuelCorreaAR <correa.emanuel@outlook.com>
2023-02-18 03:06:34 -0300

    issue size welcome

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='flex justify-between px-4 pt-4'>
         <h2 className='font-bold text-xl text-purple-900'>Dashboard</h2>
-        <h2>Welcome, Emanuel Correa</h2>
+        <h2>Welcome, Emanuel</h2>
     </div>
   )
 }
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -13,34 +13,34 @@ const Sidebar = ({ children }) => {
         <div className="flex flex-col items-center">
           <Link href="/">
             <div className="bg-purple-800 text-white p-3 rounded-lg inline-block hover:scale-105 ease-in duration-300">
-                <RxSketchLogo size={30}/>
+                <RxSketchLogo size={20}/>
             </div>
           </Link>
           <span className="border-b-[1px] border-gray-100 w-full p-2"></span>
           <Link href="/">
             <div className="bg-gray-100 hover:bg-purple-200 my-4 p-3 rounded-lg inline-block hover:scale-105 ease-in duration-300">
-                <RxDashboard size={30}/>
+                <RxDashboard size={20}/>
             </div>
           </Link>
           <Link href="/customers">
             <div className="bg-gray-100 hover:bg-purple-200 my-4 p-3 rounded-lg inline-block hover:scale-105 ease-in duration-300">
-                <RxPerson size={30}/>
+                <RxPerson size={20}/>
             </div>
           </Link>
           <Link href="/orders">
             <div className="bg-gray-100 hover:bg-purple-200 my-4 p-3 rounded-lg inline-block hover:scale-105 ease-in duration-300">
-                <HiOutlineShoppingBag size={30}/>
+                <HiOutlineShoppingBag size={25}/>
             </div>
           </Link>
           <Link href="/">
             <div className="bg-gray-100 hover:bg-purple-200 my-4 p-3 rounded-lg inline-block hover:scale-105 ease-in duration-300">
-                <FiSettings size={30}/>
+                <FiSettings size={25}/>
             </div>
           </Link>
         </div>
         <Link href="/">
             <div className="bg-purple-800 text-white my-4 p-3 rounded-lg inline-block hover:scale-105 ease-in duration-300">
-                <RxAvatar size={30}/>
+                <RxAvatar size={25}/>
             </div>
           </Link>
       </div>
